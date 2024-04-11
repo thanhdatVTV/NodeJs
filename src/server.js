e65030488309f5d6c3105a33ce60fd4895db5983
thanhdatVTV <106882741+thanhdatVTV@users.noreply.github.com>
2024-04-11 18:17:32 +0700

    customize code

--- src/server.js
+++ src/server.js
@@ -3,6 +3,7 @@ const express = require('express');
 const configViewEngine = require('./config/viewEngine');
 const webRoutes = require('./routes/web');
 const { db } = require('./firebase.js');
+const LecturerServices = require('./services/LecturerService');
 
 //console.log(process.env);
 
@@ -43,133 +44,13 @@ app.use(express.json());
 // }));
 
 //===>Giang vien<=== BEGIN//
+app.get('/api/lecturers/get-list', LecturerServices.getList);
 
-app.get('/api/lecturers/get-list', async (req, res) => {
-  try {
-    // Parse query parameters
-    const { keyword, pageNumber = 1, perPage = 10 } = req.query;
+app.post('/api/lecturers/add-lecturers', LecturerServices.addLecturer);
 
-    // Ensure pageNumber and perPage are parsed as integers
-    const parsedPageNumber = parseInt(pageNumber);
-    const parsedPerPage = parseInt(perPage);
+app.post('/api/lecturers/update-lecturer', LecturerServices.updateLecturer);
 
-    let lecturersRef = db.collection('tbl_GiangVien');
-
-    // Apply keyword filter if provided
-    if (keyword) {
-      lecturersRef = lecturersRef.where('fieldName', '==', keyword); // Replace "fieldName" with the actual field name
-    }
-
-    // Add filter condition to exclude documents where isDelete is true
-    lecturersRef = lecturersRef.where('IsDelete', '!=', true);
-
-    // Calculate the starting index based on parsedPageNumber and parsedPerPage
-    const startIndex = (parsedPageNumber - 1) * parsedPerPage;
-
-    // Fetch a page of documents
-    const snapshot = await lecturersRef.limit(parsedPerPage).offset(startIndex).get();
-
-    if (snapshot.empty) {
-      return res.status(404).send('No documents found');
-    }
-
-    const lecturers = [];
-    snapshot.forEach((doc) => {
-      lecturers.push({
-        id: doc.id,
-        data: doc.data(),
-      });
-    });
-
-    res.status(200).send(lecturers);
-  } catch (error) {
-    console.error('Error getting documents: ', error);
-    res.status(500).send('Internal Server Error');
-  }
-});
-
-// Tạo mới giảng viên
-app.post('/api/lecturers/add-lecturers', async (req, res) => {
-  try {
-    const { Status, UserUpdated, TenGV, DateUpdated, MaGV, UserCreated, IsDelete, DateCreated } =
-      req.body;
-
-    const lecturerData = {
-      Status,
-      UserUpdated,
-      TenGV,
-      DateUpdated: new Date(DateUpdated._seconds * 1000 + DateUpdated._nanoseconds / 1e6), // Convert Firestore timestamp to JavaScript Date
-      MaGV,
-      UserCreated,
-      IsDelete,
-      DateCreated: new Date(DateCreated._seconds * 1000 + DateCreated._nanoseconds / 1e6), // Convert Firestore timestamp to JavaScript Date
-    };
-
-    const docRef = await db.collection('tbl_GiangVien').add(lecturerData);
-
-    res.status(201).send(`Lecturer added with ID: ${docRef.id}`);
-  } catch (error) {
-    console.error('Error adding lecturer: ', error);
-    res.status(500).send('Internal Server Error');
-  }
-});
-
-//Sua thong tin Giang Vien
-app.post('/api/lecturers/update-lecturer', async (req, res) => {
-  try {
-    const { Id, UserUpdated, TenGV, DateUpdated, MaGV } = req.body;
-
-    // Validate required parameters
-    // if (!Id || !UserUpdated || !TenGV || !DateUpdated || !MaGV) {
-    //     return res.status(400).send("Missing required parameters");
-    // }
-
-    const lecturerRef = db.collection('tbl_GiangVien').doc(Id);
-
-    // Check if the document exists
-    const doc = await lecturerRef.get();
-    if (!doc.exists) {
-      return res.status(404).send('Document not found');
-    }
-
-    // Update the document with the provided data
-    await lecturerRef.update({
-      UserUpdated,
-      TenGV,
-      DateUpdated: new Date(DateUpdated._seconds * 1000 + DateUpdated._nanoseconds / 1e6),
-      MaGV,
-    });
-
-    res.status(200).send('Lecturer information updated successfully');
-  } catch (error) {
-    console.error('Error updating lecturer information: ', error);
-    res.status(500).send('Internal Server Error');
-  }
-});
-
-app.post('/api/lecturers/delete-lecturer', async (req, res) => {
-  try {
-    const { Id } = req.body;
-
-    const lecturerRef = db.collection('tbl_GiangVien').doc(Id);
-
-    // Check if the document exists
-    const doc = await lecturerRef.get();
-    if (!doc.exists) {
-      return res.status(404).send('Document not found');
-    }
-
-    // Update the document with the provided data
-    await lecturerRef.update({
-      IsDelete: true,
-    });
-
-    res.status(200).send('Lecturer information delete successfully');
-  } catch (error) {
-    console.error('Error delete lecturer information: ', error);
-    res.status(500).send('Internal Server Error');
-  }
-});
+app.post('/api/lecturers/delete-lecturer', LecturerServices.deleteLecturer);
 
 //===>Giang vien<=== END//
 
---
+++ src/services/LecturerService.js
@@ -0,0 +1,217 @@
+// GiangVienServices.js
+
+const { db } = require('../firebase.js');
+
+async function getList(req, res) {
+    try {
+        // Parse query parameters
+        const { keyword, pageNumber = 1, perPage = 10 } = req.query;
+
+        // Ensure pageNumber and perPage are parsed as integers
+        const parsedPageNumber = parseInt(pageNumber);
+        const parsedPerPage = parseInt(perPage);
+
+        let lecturersRef = db.collection('tbl_GiangVien');
+
+        // Apply keyword filter if provided
+        if (keyword) {
+            lecturersRef = lecturersRef.where('fieldName', '==', keyword); // Replace "fieldName" with the actual field name
+        }
+
+        // Add filter condition to exclude documents where isDelete is true
+        lecturersRef = lecturersRef.where('IsDelete', '!=', true);
+
+        // Calculate the starting index based on parsedPageNumber and parsedPerPage
+        const startIndex = (parsedPageNumber - 1) * parsedPerPage;
+
+        // Fetch a page of documents
+        const snapshot = await lecturersRef.limit(parsedPerPage).offset(startIndex).get();
+
+        if (snapshot.empty) {
+            const resultViewModel = {
+                status: -1,
+                message: 'No documents found',
+                response: null,
+                totalRecord: 0
+            };
+            return res.status(404).send(resultViewModel);
+        }
+
+        const lecturers = [];
+        snapshot.forEach((doc) => {
+            lecturers.push({
+                id: doc.id,
+                data: doc.data(),
+            });
+        });
+
+        const resultViewModel = {
+            status: 1,
+            message: 'success',
+            response: lecturers,
+            totalRecord: lecturers.length // You might need to adjust this depending on your actual total record count
+        };
+
+        res.status(200).send(resultViewModel);
+    } catch (error) {
+        console.error('Error getting documents: ', error);
+        const resultViewModel = {
+            status: -1,
+            message: 'Internal Server Error',
+            response: null,
+            totalRecord: 0
+        };
+        res.status(500).send(resultViewModel);
+    }
+}
+
+async function addLecturer(req, res) {
+    try {
+        const { Status, UserUpdated, TenGV, DateUpdated, MaGV, UserCreated, IsDelete, DateCreated } =
+            req.body;
+
+        // Check if MaGV already exists
+        const existingLecturer = await db.collection('tbl_GiangVien').where('MaGV', '==', MaGV).get();
+        if (!existingLecturer.empty) {
+            const resultViewModel = {
+                status: 0,
+                message: 'MaGV already exists',
+                response: null,
+                totalRecord: 0
+            };
+            return res.status(200).send(resultViewModel);
+        }
+
+        const lecturerData = {
+            Status,
+            UserUpdated,
+            TenGV,
+            DateUpdated: new Date(DateUpdated._seconds * 1000 + DateUpdated._nanoseconds / 1e6), // Convert Firestore timestamp to JavaScript Date
+            MaGV,
+            UserCreated,
+            IsDelete,
+            DateCreated: new Date(DateCreated._seconds * 1000 + DateCreated._nanoseconds / 1e6), // Convert Firestore timestamp to JavaScript Date
+        };
+
+        const docRef = await db.collection('tbl_GiangVien').add(lecturerData);
+
+        const resultViewModel = {
+            status: 1,
+            message: 'Thêm mới thành công',
+            response: `Lecturer added with ID: ${docRef.id}`,
+            totalRecord: 1
+        };
+
+        res.status(201).send(resultViewModel);
+    } catch (error) {
+        console.error('Error adding lecturer: ', error);
+        const resultViewModel = {
+            status: -1,
+            message: 'Internal Server Error',
+            response: null,
+            totalRecord: 0
+        };
+        res.status(500).send(resultViewModel);
+    }
+}
+
+async function updateLecturer(req, res) {
+    try {
+        const { Id, UserUpdated, TenGV, DateUpdated, MaGV } = req.body;
+
+        // Validate required parameters if needed
+        // if (!Id || !UserUpdated || !TenGV || !DateUpdated || !MaGV) {
+        //     return res.status(400).send("Missing required parameters");
+        // }
+
+        const lecturerRef = db.collection('tbl_GiangVien').doc(Id);
+
+        // Check if the document exists
+        const doc = await lecturerRef.get();
+        if (!doc.exists) {
+            const resultViewModel = {
+                status: 0,
+                message: 'Document not found',
+                response: null,
+                totalRecord: 0
+            };
+            return res.status(404).send(resultViewModel);
+        }
+
+        // Update the document with the provided data
+        await lecturerRef.update({
+            UserUpdated,
+            TenGV,
+            DateUpdated: new Date(DateUpdated._seconds * 1000 + DateUpdated._nanoseconds / 1e6),
+            MaGV,
+        });
+
+        const resultViewModel = {
+            status: 1,
+            message: 'Lecturer information updated successfully',
+            response: null,
+            totalRecord: 0
+        };
+
+        res.status(200).send(resultViewModel);
+    } catch (error) {
+        console.error('Error updating lecturer information: ', error);
+        const resultViewModel = {
+            status: -1,
+            message: 'Internal Server Error',
+            response: null,
+            totalRecord: 0
+        };
+        res.status(500).send(resultViewModel);
+    }
+}
+
+async function deleteLecturer(req, res) {
+    try {
+        const { Id } = req.body;
+
+        const lecturerRef = db.collection('tbl_GiangVien').doc(Id);
+
+        // Check if the document exists
+        const doc = await lecturerRef.get();
+        if (!doc.exists) {
+            const resultViewModel = {
+                status: 0,
+                message: 'Document not found',
+                response: null,
+                totalRecord: 0
+            };
+            return res.status(404).send(resultViewModel);
+        }
+
+        // Update the document with the provided data
+        await lecturerRef.update({
+            IsDelete: true,
+        });
+
+        const resultViewModel = {
+            status: 1,
+            message: 'Lecturer information delete successfully',
+            response: null,
+            totalRecord: 0
+        };
+
+        res.status(200).send(resultViewModel);
+    } catch (error) {
+        console.error('Error delete lecturer information: ', error);
+        const resultViewModel = {
+            status: -1,
+            message: 'Internal Server Error',
+            response: null,
+            totalRecord: 0
+        };
+        res.status(500).send(resultViewModel);
+    }
+}
+
+module.exports = {
+    getList,
+    addLecturer,
+    updateLecturer,
+    deleteLecturer
+};
